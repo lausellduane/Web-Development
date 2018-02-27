20d5b570ef134d03fba5f7935c49ed2ae5d8f5d1
Duane Lausell <lausellduane@gmail.com>
2018-02-27 13:24:29 -0500

    Update insertattendance.py

--- projects/metrics/init/insertattendance.py
+++ projects/metrics/init/insertattendance.py
@@ -11,19 +11,19 @@ collection.delete_many({})
 
 new_attendance = [
 #Safety
-{ "TEAM": "amlis", "YEAR": "2017", "MONTH": "8", "STAFF": "Carlos A.", "W1A": "green", "W1B": "red",
+{ "TEAM": "amlis", "YEAR": "2017", "MONTH": "8", "STAFF": "Carlos", "W1A": "green", "W1B": "red",
     "W2A": "green",   "W2B": "white",  "W3A": "white", "W3B": "white", "W4A": "white",  "W4B": "white",
     "W5A": "white", "W5B": "white"},
-{ "TEAM": "amlis", "YEAR": "2017", "MONTH": "8", "STAFF": "Alexis",    "W1A": "blue",  "W1B": "green",
+{ "TEAM": "amlis", "YEAR": "2017", "MONTH": "8", "STAFF": "Alex",    "W1A": "blue",  "W1B": "green",
     "W2A": "green",   "W2B": "white",  "W3A": "white", "W3B": "white", "W4A": "white",  "W4B": "white",
     "W5A": "white", "W5B": "white"},
-{ "TEAM": "amlis", "YEAR": "2017", "MONTH": "8", "STAFF": "Otto",      "W1A": "green", "W1B": "green",
+{ "TEAM": "amlis", "YEAR": "2017", "MONTH": "8", "STAFF": "Omar",      "W1A": "green", "W1B": "green",
     "W2A": "green",   "W2B": "white",  "W3A": "white", "W3B": "white", "W4A": "white",  "W4B": "white",
     "W5A": "white", "W5B": "white"},
-{ "TEAM": "amlis", "YEAR": "2017", "MONTH": "8", "STAFF": "Burgos",    "W1A": "red",   "W1B": "red",
+{ "TEAM": "amlis", "YEAR": "2017", "MONTH": "8", "STAFF": "Rodriguez",    "W1A": "red",   "W1B": "red",
     "W2A": "blue",    "W2B": "white",  "W3A": "white", "W3B": "white", "W4A": "white",  "W4B": "white",
     "W5A": "white", "W5B": "white"},
-{ "TEAM": "amlis", "YEAR": "2017", "MONTH": "8", "STAFF": "Victor",    "W1A": "green", "W1B": "green",
+{ "TEAM": "amlis", "YEAR": "2017", "MONTH": "8", "STAFF": "Jose",    "W1A": "green", "W1B": "green",
     "W2A": "green",   "W2B": "white",  "W3A": "white", "W3B": "white", "W4A": "white",  "W4B": "white",
     "W5A": "white", "W5B": "white"},
 
